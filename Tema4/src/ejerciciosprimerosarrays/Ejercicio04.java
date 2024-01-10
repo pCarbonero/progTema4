cd3fbe23d3ba6469299d4e66e5f5a67ab92a6c2e
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-10 08:47:01 +0100

    [new] ejercicio 4

---
+++ Tema4/src/ejerciciosprimerosarrays/Ejercicio04.java
@@ -0,0 +1,23 @@
+package ejerciciosprimerosarrays;
+
+import java.util.Arrays;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+		// creamos un array de 12 
+		int num[] = new int[12];
+		
+		// le damos un valor a algunos indices del array
+		num[0] = 39;
+		num[1] = -2;
+		num[4] = 0;
+		num[6] = 14;
+		num [8] = 5;
+		num[9] = 120;
+		
+		System.out.println(Arrays.toString(num));
+
+	}
+
+}
