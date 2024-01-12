4c29669af4e55378f5f4b98cf9eba1cae98d0226
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-12 14:15:16 +0100

    [new] ej 02 incompleto

---
+++ Tema4/src/ejerciciosclasearrays/Ejercicio02.java
@@ -0,0 +1,16 @@
+package ejerciciosclasearrays;
+
+import java.util.Arrays;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int prueba[] = new int[55];
+
+		
+		System.out.println("");
+		//System.out.println(Arrays.toString(prueba));
+	}
+
+}
