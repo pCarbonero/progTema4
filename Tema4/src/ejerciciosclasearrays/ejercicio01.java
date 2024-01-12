d8254d4f19387d54750898fd073cc7438052d2c3
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-12 13:19:31 +0100

    [new] ej 01 arrays

---
+++ Tema4/src/ejerciciosclasearrays/ejercicio01.java
@@ -0,0 +1,38 @@
+package ejerciciosclasearrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicio01 {
+
+	public static void main(String[] args) {
+		// Variable para lso dos números que pedimos al usuario
+		int n, m;
+		// Creamos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos el valro de n
+		System.out.println("Dime un valor para el tamaño de la tabla ");
+		// Guardamos el valro en al variable n
+		n = sc.nextInt();	
+		
+		//Pedimos el valro de m
+		System.out.println("Dime un valor para rellenar al tabla ");
+		// Guardamos el valro en al variable m
+		m = sc.nextInt();	
+		
+		
+		// creamos array de tamaño n
+		int tabla[] = new int[n];
+		
+		// rellenamos la tabla con m
+		Arrays.fill(tabla, m);
+		
+		// imprimimos la tabla
+		System.out.println(Arrays.toString(tabla));
+		
+		// cerramso el escaner
+		sc.close();
+	}
+
+}
