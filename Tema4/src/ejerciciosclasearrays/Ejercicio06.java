fef90ccf7949463cc9e7d4a4c2a3c071938b3244
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-14 17:25:44 +0100

    [upd] ejercicio06 fin

--- Tema4/src/ejerciciosclasearrays/Ejercicio06.java
+++ Tema4/src/ejerciciosclasearrays/Ejercicio06.java
@@ -1,5 +1,6 @@
 package ejerciciosclasearrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio06 {
@@ -9,6 +10,10 @@ public class Ejercicio06 {
 		int tabla[] = new int[1000];
 		// creamos el escaner 
 		Scanner sc = new Scanner(System.in);
+		// variable para almacenar el numero que queremos buscar
+		int num;
+		// contador para la cantidad de veces que se ha generado el número
+		int contador = 0;
 		
 		// creamos el bucle para rellenar la tabla
 		for (int i = 0; i < tabla.length; i++) {
@@ -17,6 +22,23 @@ public class Ejercicio06 {
 		
 		// pedimos al usuario un número
 		System.out.println("Dime un número ");
+		num = sc.nextInt();
+		
+		// bucle que buscará en cada índice del array si está el número que buscamos
+		for (int i = 0; i < tabla.length; i++) {
+			// si el valor del indice de la tabla es igual a num aumentamos en 1 el contador 
+			if (num == tabla[i]) {
+				contador++;
+			}
+		}// fin bucle
+		
+		// imprimimos los resultdos
+		System.out.println(Arrays.toString(tabla));
+		
+		System.out.println("Se ha el número " + num + " un total de " + contador + " veces");
+		
+		// cerramos el escaner
+		sc.close();
 	}
 
 }
