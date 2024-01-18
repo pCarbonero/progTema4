7e0a09d11dc8ceea59faf562c42fd57ee8fd4cc5
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-18 13:43:51 +0100

    [new] creacion ej06

---
+++ Tema4/src/ejerciciosfuncionesarrays/Ejercicio06.java
@@ -0,0 +1,14 @@
+package ejerciciosfuncionesarrays;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int[] suma(int t[], int numElementos) {
+		return t;
+		
+	}
+}
