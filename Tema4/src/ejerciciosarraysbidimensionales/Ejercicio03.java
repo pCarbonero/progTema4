5ce882c9ef68554a8488125efa829dae6fadf3ae
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-30 20:41:50 +0100

    [new] ejercicio 03 bidi

--- Tema4/src/ejerciciosarraysbidimensionales/Ejercicio03.java
+++ Tema4/src/ejerciciosarraysbidimensionales/Ejercicio03.java
@@ -3,8 +3,35 @@ package ejerciciosarraysbidimensionales;
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// creamos la tabla bidimiensional
+		int[][] tabla = new int[5][5];
 
+		rellenaTabla(tabla);
+		imprimeTabla(tabla);
 	}
+
+	public static void rellenaTabla(int[][] tabla) {
+
+		for (int n = 0; n < tabla.length; n++) {
+			for (int m = 0; m < tabla[n].length; m++) {
+				tabla[n][m] = 10 * n + m;
+
+			} // fin bucle m
+
+		} // fin bucle n
+	}// fin void
+	
+	public static void imprimeTabla(int[][] tabla) {
+
+		for (int n = 0; n < tabla.length; n++) {
+			for (int m = 0; m < tabla[n].length; m++) {
+				System.out.print(tabla[n][m] + "\t");
+			} // fin bucle m
+			System.out.println(" ");
+		} // fin bucle n
+	}// fin imprime
+	
+	
+	
 
 }
