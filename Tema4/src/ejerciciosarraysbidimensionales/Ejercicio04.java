114d7725ac353f87e3187c50c4fa2b00247be677
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-30 20:55:18 +0100

    [new] ejercicio04 bidimension

---
+++ Tema4/src/ejerciciosarraysbidimensionales/Ejercicio04.java
@@ -0,0 +1,36 @@
+package ejerciciosarraysbidimensionales;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+		// Creamos la tabla 10x10
+		int[][] tabla = new int[10][10];
+
+		// llamamos a la tabla y la igualamos a la tabla del main
+		
+		tabla.equals(tablasDeMultiplicar(tabla));
+
+		
+		// imrpimimos el resultado
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				System.out.print(tabla[i][j] + "\t");
+			} // fin bucle i
+			System.out.println(" ");
+		} // fin bucle j
+
+	}// fin main
+
+	static int[][] tablasDeMultiplicar(int[][] tabla) {
+		// int[][] tablaMultiplicar = new int[10][10];
+
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				tabla[i][j] = (i+1) * (j+1);
+			}
+		}
+
+		return tabla;
+	}
+
+}// fin class
