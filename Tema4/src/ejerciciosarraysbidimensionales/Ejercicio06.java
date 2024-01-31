95abc196625ef6b56f9b3ec58e0c78f99124ab0f
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-31 10:13:31 +0100

    [new] ejercicio 06 bidi

---
+++ Tema4/src/ejerciciosarraysbidimensionales/Ejercicio06.java
@@ -0,0 +1,62 @@
+package ejerciciosarraysbidimensionales;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		// creamos al tabla
+		int [][] tabla = new int[6][10];
+		// creamos un objeto de la clase random 
+		Random rn = new Random();
+		
+		// bucle para rellenar la tabla
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				tabla[i][j] = rn.nextInt(0, 1001);
+			}
+		}
+		
+		/*// imrpimimos el resultado
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				System.out.print(tabla[i][j] + "\t");
+			} // fin bucle i
+			System.out.println(" ");
+		} // fin bucle j*/
+		
+		//maxAndMin(tabla);	
+		
+		System.out.println(Arrays.toString(maxAndMin(tabla)));
+
+	}// fin main
+	
+	public static int[] maxAndMin(int[][] tabla) {
+		// creamos yabla de una dimension
+		int[] maxAndMin = new int[2];
+		// variable para el numer maximo y minimo
+		int max = Integer.MIN_VALUE;
+		int	min = Integer.MAX_VALUE;
+		
+		// calculamos el max y min
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				if (tabla[i][j] <= min) {
+					min = tabla[i][j];
+				}// if st
+				
+				if (tabla[i][j] >= max) {
+					max = tabla[i][j];
+				}// if st
+			}
+		}// fin bucle
+		
+		
+		maxAndMin[0]= max;
+		maxAndMin[1]= min;
+		
+		return maxAndMin;
+		}
+
+}
