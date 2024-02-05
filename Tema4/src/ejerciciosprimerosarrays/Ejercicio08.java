ba6a00cc785beea227ae30c43b231e512f78c413
pCarbonero <pablo.carbonero@iesnervion.es>
2024-02-05 17:38:21 +0100

    [new] ejercicio 08 primerps

--- Tema4/src/ejerciciosprimerosarrays/Ejercicio08.java
+++ Tema4/src/ejerciciosprimerosarrays/Ejercicio08.java
@@ -1,10 +1,36 @@
 package ejerciciosprimerosarrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// creamos un array de tamaño 100
+		int[] tabla = new int[100];
+		// creamos el escaner
+		Scanner sc = new Scanner(System.in);
+		// creamos variable aora almacenar el número que buscamos
+		int numBuscado;
+		
+		// bucle para rellenar la tablña con numeros aleatorios
+		for (int i = 0; i < tabla.length; i++) {
+			tabla[i] = (int)((1+Math.random()*10));			
+		}
+		
+		System.out.println(Arrays.toString(tabla));
+		
+		// pedimos al usuario el número
+		System.out.println("Dime un número y lo biscamremos en la tabla ");
+		numBuscado = sc.nextInt();
+		
+		// bucle que busca el número e imprime el resultado
+		for (int i = 0; i < tabla.length; i++) {
+			if (tabla[i] == numBuscado) {
+				System.out.println(numBuscado + " se encuentra en la posición " + i);
+			}		
+		}
 
-	}
+	}	
 
 }
