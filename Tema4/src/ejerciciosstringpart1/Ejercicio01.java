d29d3b900f4737e2bf3e782588bb9f2c03cac13a
pCarbonero <pablo.carbonero@iesnervion.es>
2024-02-07 09:42:07 +0100

    [new] eje 01 y 02 string

---
+++ Tema4/src/ejerciciosstringpart1/Ejercicio01.java
@@ -0,0 +1,22 @@
+package ejerciciosstringpart1;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		// creamos los strings
+		String  frase1 = "Understand I bow to nothing ";
+		String frase2 = "Comprehend I'm through with running";	
+		// trign para el resultado
+		String res = "Son igual de largas ";
+		
+		// creamos un if else para comprobar si ambas cadenas son igual de largas
+		if (frase1.length() != frase2.length()) {
+			// si no son igual de larags utilizamos un ternario para comprobar cual es mas larga
+			res = (frase1.length() > frase2.length()) ? "La primera es más larga":"La segunda es más larga";
+			System.out.println(res);
+		}
+		// imrpimimos el resultado
+		System.out.println(res);
+
+	}
+}
---
+++ Tema4/src/ejerciciosstringpart1/Ejercicio02.java
@@ -0,0 +1,10 @@
+package ejerciciosstringpart1;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// creamos el escanner
+
+	}
+
+}
