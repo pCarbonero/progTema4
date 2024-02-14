14eedf8cf688b919c34da11a614870e62944bd2c
pCarbonero <pablo.carbonero@iesnervion.es>
2024-02-14 09:07:55 +0100

    [new] ej04

---
+++ Tema4/src/ejerciciosstringpart1/Ejercicio04.java
@@ -0,0 +1,31 @@
+package ejerciciosstringpart1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+		// creamos el string de la frase que hay que leer
+		String frase;
+		// creamos el escaner
+		Scanner sc = new Scanner(System.in);
+		// creamos un array para ordenar la cadena alfabticamente
+		String[] palabras;
+		
+		// pedimos al usuario la frase
+		System.out.println("Intoduce una frase ");
+		frase = sc.nextLine();
+		
+		// relleanmos el array con cada palabra
+		palabras = frase.split(" ");	
+		
+		// ordenamos el array
+		Arrays.sort(palabras);
+		
+		// imrpimimos el array ordenado
+		System.out.println(Arrays.toString(palabras));
+
+	}
+
+}
