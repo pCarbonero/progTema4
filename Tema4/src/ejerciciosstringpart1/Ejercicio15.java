2b262919c578555ca29031a862babf454fb518c4
pCarbonero <pablo.carbonero@iesnervion.es>
2024-02-19 17:07:26 +0100

    [upd] ejercicio15

--- Tema4/src/ejerciciosstringpart1/Ejercicio15.java
+++ Tema4/src/ejerciciosstringpart1/Ejercicio15.java
@@ -11,6 +11,8 @@ public class Ejercicio15 {
 		String palabra = "";
 		// cadena para el anagrama
 		String anagrama = "";
+		// string para la respuesta
+		String res = "";
 		// scanner
 		Scanner sc = new Scanner(System.in);
 
@@ -18,39 +20,57 @@ public class Ejercicio15 {
 		System.out.println("ESCRIBE PALABRA");
 		palabra = sc.nextLine();
 
+		do {
 		anagrama = desordena(palabra);
+		//System.out.println(desordena(palabra));
 		System.out.println(anagrama);
+		// pedimos al jugador 2 por una palabra
+		System.out.println("¿Cuál crees que es la palabra original?");
+		res = sc.nextLine();
+		// llamamos de nuevo a la función
+		anagrama = desordena(palabra);
+		}while (!palabra.equals(res)) ;
 
+		// IMPRIMIMOS MENSAJE DE FINAL
+		System.out.println("Es correcto");
+
+		// cerramps el escaner
 		sc.close();
 
 	}
 
 	static String desordena(String palabra) {
+		// string para el anagrama
 		String anagrama = "";
 		// Convertir el texto a un array de caracteres
 		char[] caracteres = palabra.toCharArray();
-		//
+		// int para el indice aleatorio
 		int indiceAleatorio;
-		//
+		// objeto de la clase random
 		Random rand = new Random();
-		//
-		char[] caracteres2 = new char[caracteres.length];
+		// creamos un array con la longitud de la palabra
+		char[] aux = new char[caracteres.length];
+		// llenamos el array con espacios
+		Arrays.fill(aux, ' ');
 
-		//
+		// bucle que va a rellenar el array del anagrama
 		for (int i = 0; i < caracteres.length; i++) {
-			indiceAleatorio = rand.nextInt(i + 1);
-
-			if(caracteres[indiceAleatorio] != ' ') {
-				caracteres2[i] = caracteres[indiceAleatorio];
-				caracteres[indiceAleatorio] = ' ';
-				System.out.println(caracteres[indiceAleatorio]);
+			// generamo el indice aleatorio
+			indiceAleatorio = rand.nextInt(0, caracteres.length);
+			// si el indice del array aux no es un espacio es que esa letra ya se a colocado
+			// para que funcione bien el bucle while buscará un nuevo indice qeu no esté
+			// ocupado
+			while (aux[indiceAleatorio] != ' ') {
+				// buscamos de nuevo el numero
+				indiceAleatorio = rand.nextInt(0, caracteres.length);
 			}
-
-
+			// asignamos el caracter al espacio
+			aux[indiceAleatorio] = caracteres[i];
 		}
 
-		anagrama = anagrama.valueOf(caracteres2);
 		// Convertir el array de nuevo a un String
+		anagrama = anagrama.valueOf(aux);
+		// devolvemos la palabra transformada
 		return anagrama;
 	}
 
