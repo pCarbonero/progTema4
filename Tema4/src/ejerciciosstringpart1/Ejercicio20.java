2bee91051f8739cd84284a711b21b98ebd77c4c6
pCarbonero <pablo.carbonero@iesnervion.es>
2024-02-22 19:38:57 +0100

    [new] ej 20

--- Tema4/src/ejerciciosstringpart1/Ejercicio20.java
+++ Tema4/src/ejerciciosstringpart1/Ejercicio20.java
@@ -1,10 +1,31 @@
 package ejerciciosstringpart1;
 
+import java.util.Arrays;
+
 public class Ejercicio20 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] separacion = separador("Destornillador", 4);
+		System.out.println(Arrays.toString(separacion));
 
+	}
+
+	static String[] separador(String palabra, int numLetras) {
+		int tam = (int) Math.ceil((double) palabra.length() / numLetras);
+
+		String[] separacion = new String[tam];
+		// System.out.println(Arrays.toString(separacion));
+		for (int i = 0; i < separacion.length; i++) {
+
+			if (numLetras * (i + 1) > palabra.length()) {
+				separacion[i] = palabra.substring(numLetras * i);
+			} else {
+				separacion[i] = palabra.substring(numLetras * i, numLetras * (i + 1));
+			}
+		}
+
+		return separacion;
 	}
 
 }
